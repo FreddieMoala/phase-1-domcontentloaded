1baf0cdf3ff07db94c3a803cc8be50ffdcaa5d66
Siaosi Moala <loto_toa@hotmail.com>
2024-04-16 16:18:38 +1000

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM fully loaded and parsed");
+    doSomething();
+});
+
+function doSomething() {
+    document.getElementById('text').textContent = "This is really cool!";
+}
